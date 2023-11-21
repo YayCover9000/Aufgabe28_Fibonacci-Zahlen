aaf451a82c89e41055928989efc5ce9f784dd1c3
YayCover9000 <yassuanf@googlemail.com>
2023-11-21 19:14:32 +0100

    no working

--- Aufgabe28_FibonacciZahlen.java
+++ Aufgabe28_FibonacciZahlen.java
@@ -1,4 +1,17 @@
 
 public class Aufgabe28_FibonacciZahlen {
-
+	public static void main(String [] args) {
+		int f = 1;
+		int i = 0;
+		int x =0;
+		System.out.println(rekursivFibonacci(f,i,x));
+	}
+	private static int rekursivFibonacci(int f, int i, int x) {
+		while(x<8) {
+			f = rekursivFibonacci(f+i,i = f,x);
+			x++;
+		}
+		return f;
+	}
+	
 }
